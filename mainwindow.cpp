04c2a5ea65c184de5d36ac8ba2e754ec68280105
david1340 <davidnike80@gmail.com>
2021-06-03 00:18:25 -0300

    Primeiro commit

---
+++ mainwindow.cpp
@@ -0,0 +1,309 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QStringList>
+#include <QLineEdit>
+#include <stdlib.h>
+#include <QString>
+#include <QIODevice>
+#include <iostream>
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+
+    //chama a função a cada 10 milisegundos
+    /*
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout,
+           this, &MainWindow::read_Data);
+    timer->start(10);
+    */
+    ui->setupUi(this); //ja veio por default do QT
+    this->serialPort = new QSerialPort; //instanciando porta serial
+    QStringList portas; //Lista de Strings
+    const auto serialPortInfos = QSerialPortInfo::availablePorts();//método estático
+    int i =0;
+    //alimentando o combox com as informações da porta serial
+    for (const QSerialPortInfo &serialPortInfo : serialPortInfos)
+    {
+        //Listando as portas disponíveis
+        portas.insert(i, serialPortInfo.portName());
+        i++;
+    }
+    ui->comboBox->insertItems(0, portas); //Dessa forma funciona para qualquer sistema operacional
+    connect(ui->Connect_Disconect,SIGNAL(clicked(bool)),this,SLOT(connectToSerial())); // conexão serial
+    ui->label->setText("Desconectado");
+    ui->Baud_Rate->setText(""); //limpando label do Baud Rate
+}
+
+
+void MainWindow::checkSerial(){  // funcão criada para tentar reconectar
+    //const auto serialPortInfos = QSerialPortInfo::availablePorts();
+    auto serialPortInfos = QSerialPortInfo::availablePorts();
+    QStringList portas;
+    portas.clear();
+    int i =0;
+    for (const QSerialPortInfo &serialPortInfo : serialPortInfos){
+        portas.insert(i, serialPortInfo.portName());
+        i++;
+    }
+    ui->comboBox->clear();
+    ui->comboBox->insertItems(0, portas);
+}
+
+void MainWindow::connectToSerial(){
+    if(this->serialPort->isOpen()){ // se conectado
+        closeSerialPort();
+        //this->serialPort->close();
+        ui->label->setText("Desconectado");
+        ui->Connect_Disconect->setText("Conectar");
+        ui->Baud_Rate->setText(""); //limpando label do Baud Rate      
+        return;
+    }
+    // Parâmetros de Abertura da Porta Serial
+    if(ui->comboBox->count()>=0){
+    this->serialPort->setPortName(ui->comboBox->currentText());
+    this->serialPort->setBaudRate(QSerialPort::Baud115200);
+    this->serialPort->setDataBits(QSerialPort::Data8);
+    this->serialPort->setParity(QSerialPort::NoParity);
+    this->serialPort->setStopBits(QSerialPort::OneStop);
+    this->serialPort->setFlowControl(QSerialPort::NoFlowControl);
+    }
+
+    if(this->serialPort->open(QIODevice::ReadWrite)){ // Verificação se a porta realmente abriu        
+        ui->label->setText("Conectado");
+        ui->Baud_Rate->setText("Communication Paramaters\n"
+                               "Baudrate: 115200\n"
+                               "Data bits: 8\n"
+                               "Parity: None\n"
+                               "Stop bits: 1\n"
+                               "Flow control: None");
+
+        ui->Connect_Disconect->setText("Desconectar");
+
+        flag_comunicacao = !flag_comunicacao; // altera o flag de comunicação 0 - SemComunicação e 1 - ComComunicação
+
+        return;
+    }
+    ui->label->setText("Falha ao tentar conetar");
+    ui->Connect_Disconect->setText("Conectar");
+    this->serialPort->close();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::closeSerialPort()
+{
+    if (this->serialPort->isOpen()){
+        this->serialPort->close();
+        flag_comunicacao = !flag_comunicacao; // altera o flag de comunicação 0 - SemComunicação e 1 - ComComunicação
+    }
+}
+
+//void MainWindow::write_Data(const QByteArray &data)
+void MainWindow::write_Data()
+{
+
+    int index = ui->Select_Robot->currentIndex(); //Seleciona o Index
+    for(int i=0; i<11;i++){
+        write_buf[i] = C0;//C0; // ZERA O BUFFER DE ESCRITA INTEIRO
+    }
+    write_buf[0] = (unsigned char) (250+index); // ID DO ROBÔ QUE RECEBERÁ A MENSAGEM
+    write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value());
+    write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value());
+    //Buffer Completo
+    //1 3 5 7 9
+    //0 1 2 3 4
+    QByteArray write_buf3;
+    write_buf3 = QByteArray::fromRawData((char*) write_buf,11);
+
+    //write_buf3 = QByteArray::fromRawData(write_buf,11);
+    if (flag_comunicacao)
+    {
+        this->serialPort->write(write_buf3);
+        //this->serialPort->write(write_buf);
+       //qint64 i = QIODevice::writeData(const char 'a',11);
+    }
+
+    const QString texto = write_buf3;
+    ui->lineEdit->setText(texto);
+
+    if(int(write_buf3.at(0) >= 0)){
+        ui->spinBox_texte->setValue((int)write_buf3.at(0));
+    }
+    else{
+        ui->spinBox_texte->setValue(-(128 + (int)write_buf3.at(0)));
+        //ui->spinBox_texte->setValue((int)(unsigned char)write_buf3.at(1));
+    }
+}
+
+
+void MainWindow::read_Data()
+{
+
+    int index = ui->Select_Robot->currentIndex();
+    //const QByteArray datareceive = this->serialPort->readAll();
+    if (flag_comunicacao)
+    {
+        if(this->serialPort->bytesAvailable()){
+            QByteArray read_buf = this->serialPort->read(11);
+            const QString texto = read_buf;
+            ui->lineEdit_2->setText(texto);
+
+            if(int(read_buf.at(2) >= 0)){
+                ui->spinBox_texte_2->setValue((int)read_buf.at(2));
+            }
+            else{
+                ui->spinBox_texte_2->setValue(-(128 + (int)read_buf.at(2)));
+          }
+        }
+    }
+}
+
+
+
+
+
+/* CONVERSÃO DOS DADOS ENVIADOS OU RECEBIDOS  */
+unsigned char MainWindow::converter_write (int x)
+{
+    //OBSERVAÇÃO: SÓ FUNCIONA PRA VALORES MENORES QUE 127, MAS O PROTOCOLO É DE 0 A 100 COM BIT DE SINAL
+
+    unsigned char c = C0; //Inicializando com zero
+    if (x >= 0)
+    {
+        c = (unsigned char) x;
+    }
+    else
+    {
+        c = (unsigned char) (abs(x)+128); //Módulo + o bit de sinal
+    }
+    return c;
+}
+
+int MainWindow::converter_read (unsigned char c)
+{
+    int x = 0; //Inicializando com zero
+    if (((int)c) >= 128)
+        x = -(((int)c) - 128);
+    else
+        x = (int)c;
+    return x;
+}
+
+
+
+
+
+void MainWindow::initActionsConnections()
+{
+
+}
+
+void MainWindow::showStatusMessage(const QString &message)
+{
+    //m_status->setText(message);
+    //ele que exibe se está conectado ou não
+}
+
+
+void MainWindow::on_Connect_Disconect_clicked()
+{
+    //fica vazio mesmo
+    //referente ao conect e disconect
+}
+
+void MainWindow::on_comboBox_activated(int index)
+{
+    //fica vazio mesmo
+    //é referente ao botão de conecte e disconect
+}
+
+void MainWindow::on_CheckSerial_Button_clicked()
+{
+   checkSerial();
+}
+
+void MainWindow::on_Read_clicked()
+{
+    read_Data();
+}
+
+void MainWindow::on_Write_clicked()
+{
+    write_Data();
+}
+
+
+// Controle de Velocidade para Teste
+void MainWindow::on_spinBox_vel_R_valueChanged(int arg1)
+{
+    ui->verticalSlider_vel_R->setValue(arg1);
+}
+
+void MainWindow::on_spinBox_vel_L_valueChanged(int arg1)
+{
+    ui->verticalSlider_vel_L->setValue(arg1);
+}
+
+void MainWindow::on_verticalSlider_vel_R_valueChanged(int value)
+{
+    ui->spinBox_vel_R->setValue(value);
+}
+
+void MainWindow::on_verticalSlider_vel_L_valueChanged(int value)
+{
+    ui->spinBox_vel_L->setValue(value);
+}
+
+void MainWindow::on_Select_Robot_activated(int index)
+{
+/*
+    for(int i=0; i<11;i++){
+        write_buf[i] = C0; // ZERA O BUFFER DE ESCRITA INTEIRO
+    }
+
+    write_buf[0] = (unsigned char) (250+index); // ID DO ROBÔ QUE RECEBERÁ A MENSAGEM
+
+    write_buf[2*index] = converter_write(ui->verticalSlider_vel_L->value());
+    write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_R->value());
+*/
+}
+
+void MainWindow::on_spinBox_texte_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_spinBox_texte_2_valueChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_Girar_clicked()
+{
+  QThread::msleep(10);
+  ui->verticalSlider_vel_R->setValue(100);
+  ui->verticalSlider_vel_L->setValue(100);
+  write_Data();
+
+  //serialPort->waitForReadyRead(100);
+  QThread::msleep(5000);
+//  pthread_cond_wait()
+    /*
+  serialPort->waitForBytesWritten(10);
+  read_Data();
+ // QThread::usleep(10);
+ QThread::msleep(1000);
+  ui->verticalSlider_vel_L->setValue(0);
+  write_Data();
+  //serialPort->waitForReadyRead(100);
+ QThread::msleep(100);
+  read_Data();*/
+}
---
+++ mainwindow.h
@@ -0,0 +1,106 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDebug>
+#include <QStringList>
+#include <string.h>
+#include <QTimer>
+#include <stdio.h>
+#include <QThread>
+//Só é necessário para a comunicação Serial o QSerialPortInfo e o QSerialPort
+
+/* MANDAR VELOCIDADE NULA SEM PRECISAR DO CAST SEMPRE */
+
+//#define C0 (unsigned char)0
+#define C0 (char)0
+//#define C0 '0'
+/* DEFINES RELATIVOS À POSIÇÃO NO BUFFER DAS VELOCIDADES ENVIADAS */
+
+#define L1 1
+#define R1 2
+#define L2 3
+#define R2 4
+#define L3 5
+#define R3 6
+#define L4 7
+#define R4 8
+#define L5 9
+#define R5 10
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class QSerialPort;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    QSerialPort *serialPort; //reservando mémoria para um objeto do tipo QSerialPORT
+
+    unsigned char converter_write (int x);
+    int converter_read (unsigned char c);
+    void enviar_buffer (void);
+    void ler_buffer (void);
+
+
+private slots:
+    void on_Connect_Disconect_clicked();
+
+    void on_comboBox_activated(int index);
+
+    void on_CheckSerial_Button_clicked();
+
+    void on_Read_clicked();
+
+    void on_Write_clicked();
+
+    void on_spinBox_vel_R_valueChanged(int arg1);
+
+    void on_verticalSlider_vel_R_valueChanged(int value);
+
+    void on_spinBox_vel_L_valueChanged(int arg1);
+
+    void on_verticalSlider_vel_L_valueChanged(int value);
+
+
+
+    void on_spinBox_texte_valueChanged(int arg1);
+
+    void on_spinBox_texte_2_valueChanged(int arg1);
+
+    void on_Select_Robot_activated(int index);
+
+    void on_Girar_clicked();
+
+public slots:
+    void connectToSerial();
+    void checkSerial();
+    void closeSerialPort();
+    //void write_Data(const QByteArray &data);
+    void write_Data();
+    void read_Data();
+    void initActionsConnections();
+    void showStatusMessage(const QString &message);
+
+private:
+    Ui::MainWindow *ui;
+    bool flag_comunicacao = 0; // quando 1 indica comunicação ativa
+    int flag_robos = 0; // 0 = nenhum, 1-5 = robô 1-5
+    unsigned char write_buf[11] = {C0,C0,C0,C0,C0,C0,C0,C0,C0,C0,C0}; // buffer de escrita inicado com velocidades zero
+    unsigned char read_buf[11] = {C0,C0,C0,C0,C0,C0,C0,C0,C0,C0,C0};  // buffer de leitura inicado com velocidades zero
+    const QByteArray read_buf2;
+    QTimer *timer;
+
+};
+
+#endif // MAINWINDOW_H
