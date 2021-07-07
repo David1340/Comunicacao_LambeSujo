7d7695bee9d6c315f131ab231dd77d53bdf7e794
david1340 <davidnike80@gmail.com>
2021-07-06 21:48:38 -0300

    Testes de navegacao, e girar funcionando corretamente, comunicacao estavel

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -156,27 +156,21 @@ void MainWindow::read_Data()
     //const QByteArray datareceive = this->serialPort->readAll();
     if (flag_comunicacao)
     {
-        /*
-        const QString read_id2 =(QString)'10';
-        while(this->serialPort->readBufferSize()<1){} //Para aguardar o buffer chegar
-        while(this->serialPort->bytesAvailable()>=1 && read_id2 != converter_write (20)){
-                QByteArray read_id= this->serialPort->read(1);
-                const QString read_id2 = read_id;
-                //qint64 teste=this->serialPort->readBufferSize();
-                //ui->lcdNumber_id_r->display((int)teste);
-                //if(read_id2 == '20'){
-                    //ui->lcdNumber_id_r->display(read_id2);
-                //}
-           }
-        */
-        while(this->serialPort->readBufferSize()<11){
-            //serialPort->waitForReadyRead(3);
-        }
-
+        serialPort->waitForReadyRead(10);
         if(this->serialPort->bytesAvailable()>=11 ){
-            serialPort->waitForReadyRead(100);
             QByteArray read_buf = this->serialPort->readAll();
-            ui->lcdNumber_id_r->display(-converter_read(read_buf.at(0))-121);
+            int id = -converter_read(read_buf.at(0)) - 121;
+            if(id < 0 || id > 5)
+            {
+                serialPort->flush();
+                for(int i=0; i<5;i++)
+                {
+                    read_buf[(2*i) + 1] = C0; // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+                    read_buf[(2*i) + 2] = C0; // VELOCIDADE DIREITA DO ROBÔ(ID)
+                }
+
+            }
+            ui->lcdNumber_id_r->display(id);
             ui->lcdNumber_L1_r->display(converter_read(read_buf.at(1)));
             ui->lcdNumber_R1_r->display(converter_read(read_buf.at(2)));
             ui->lcdNumber_L2_r->display(converter_read(read_buf.at(3)));
@@ -196,7 +190,6 @@ void MainWindow::read_Data()
             ui->widget->graph(0)->setData(xplot, yplot1);
             ui->widget->graph(1)->setData(xplot, yplot2);
             ui->widget->replot();
-
         }
     }
 }
@@ -231,10 +224,6 @@ int MainWindow::converter_read (unsigned char c)
         x = (int)c;
     return x;
 }
-
-
-
-
 
 void MainWindow::initActionsConnections()
 {
@@ -279,16 +268,17 @@ void MainWindow::on_Write_clicked()
 void MainWindow::on_spinBox_vel_R_valueChanged(int arg1)
 {
     ui->verticalSlider_vel_R->setValue(arg1);
-
-    if(flag_navegacao==1){   // Atualiza a velocidade da navegação
+    if(flag_navegacao==1){   // Atualiza a velocidade da navegação]
+        for(int i=0; i<5;i++)
+        {
+            write_buf[(2*i) + 1] = C0; // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+            write_buf[(2*i) + 2] = C0; // VELOCIDADE DIREITA DO ROBÔ(ID)
+        }
         int index = ui->Select_Robot->currentIndex(); //Seleciona o Index
+        write_buf[0] = converter_write(index + 250);
         write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value()); // VELOCIDADE ESQUERDA DO ROBÔ(ID)
         write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value()); // VELOCIDADE DIREITA DO ROBÔ(ID)
-        serialPort->flush();
         write_Data();
-        QThread::msleep(100); // 1 ms de pause
-        //serialPort->waitForReadyRead(100);
-        //read_Data();
         read_Data();
     }
 }
@@ -296,16 +286,17 @@ void MainWindow::on_spinBox_vel_R_valueChanged(int arg1)
 void MainWindow::on_spinBox_vel_L_valueChanged(int arg1)
 {
     ui->verticalSlider_vel_L->setValue(arg1);
-
     if(flag_navegacao==1){  // Atualiza a velocidade da navegação
+        for(int i=0; i<5;i++)
+        {
+            write_buf[(2*i) + 1] = C0; // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+            write_buf[(2*i) + 2] = C0; // VELOCIDADE DIREITA DO ROBÔ(ID)
+        }
         int index = ui->Select_Robot->currentIndex(); //Seleciona o Index
+        write_buf[0] = converter_write(index + 250);
         write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value()); // VELOCIDADE ESQUERDA DO ROBÔ(ID)
         write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value()); // VELOCIDADE DIREITA DO ROBÔ(ID)
-        serialPort->flush();
         write_Data();
-        QThread::msleep(100); // 1 ms de pause
-        //serialPort->waitForReadyRead(100);
-        //read_Data();
         read_Data();
     }
 }
@@ -313,16 +304,17 @@ void MainWindow::on_spinBox_vel_L_valueChanged(int arg1)
 void MainWindow::on_verticalSlider_vel_R_valueChanged(int value)
 {
     ui->spinBox_vel_R->setValue(value);
-
     if(flag_navegacao==1){  // Atualiza a velocidade da navegação
+        for(int i=0; i<5;i++)
+        {
+            write_buf[(2*i) + 1] = C0; // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+            write_buf[(2*i) + 2] = C0; // VELOCIDADE DIREITA DO ROBÔ(ID)
+        }
         int index = ui->Select_Robot->currentIndex(); //Seleciona o Index
+        write_buf[0] = converter_write(index + 250);
         write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value()); // VELOCIDADE ESQUERDA DO ROBÔ(ID)
         write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value()); // VELOCIDADE DIREITA DO ROBÔ(ID)
-        serialPort->flush();
         write_Data();
-        QThread::msleep(100); // 1 ms de pause
-        //serialPort->waitForReadyRead(100);
-        //read_Data();
         read_Data();
     }
 }
@@ -330,32 +322,38 @@ void MainWindow::on_verticalSlider_vel_R_valueChanged(int value)
 void MainWindow::on_verticalSlider_vel_L_valueChanged(int value)
 {
     ui->spinBox_vel_L->setValue(value);
-
     if(flag_navegacao==1){   // Atualiza a velocidade da navegação
+        for(int i=0; i<5;i++)
+        {
+            write_buf[(2*i) + 1] = C0; // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+            write_buf[(2*i) + 2] = C0; // VELOCIDADE DIREITA DO ROBÔ(ID)
+        }
         int index = ui->Select_Robot->currentIndex(); //Seleciona o Index
+        write_buf[0] = converter_write(index + 250);
         write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value()); // VELOCIDADE ESQUERDA DO ROBÔ(ID)
         write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value()); // VELOCIDADE DIREITA DO ROBÔ(ID)
-        serialPort->flush();
         write_Data();
-        QThread::msleep(100); // 1 ms de pause
-        //serialPort->waitForReadyRead(100);
-        //read_Data();
         read_Data();
     }
 }
 
 void MainWindow::on_Select_Robot_activated(int index)
 {
-/*
-    for(int i=0; i<11;i++){
-        write_buf[i] = C0; // ZERA O BUFFER DE ESCRITA INTEIRO
+    if(flag_navegacao==1){   // Atualiza a velocidade da navegação
+        for(int i=0; i<5;i++)
+        {
+            write_buf[(2*i) + 1] = C0; // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+            write_buf[(2*i) + 2] = C0; // VELOCIDADE DIREITA DO ROBÔ(ID)
+        }
+        write_buf[0] = converter_write(index + 250);
+         // VELOCIDADE ESQUERDA DO ROBÔ(ID)
+        write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value());
+        // VELOCIDADE DIREITA DO ROBÔ(ID)
+        write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value());
+        serialPort->flush();
+        write_Data();
+        read_Data();
     }
-
-    write_buf[0] = (unsigned char) (250+index); // ID DO ROBÔ QUE RECEBERÁ A MENSAGEM
-
-    write_buf[2*index] = converter_write(ui->verticalSlider_vel_L->value());
-    write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_R->value());
-*/
 }
 
 void MainWindow::on_Girar_clicked()
@@ -367,14 +365,10 @@ void MainWindow::on_Girar_clicked()
         write_buf[(2*i) + 2] = converter_write(-100); // VELOCIDADE DIREITA DO ROBÔ(ID)
     }
     write_Data(); // comando para girar
-    for (int i = 0 ; i <5;i++){
-        serialPort->waitForBytesWritten(100);
-    }
     read_Data();
     for(int i=0; i<11;i++){
         if(i==0){
            write_buf[0] = (unsigned char) (index); // ID DO ROBÔ QUE RECEBERÁ A MENSAGEM
-            //write_buf[0] = converter_write(250);
         }
         else{
             write_buf[i] = C0; // ZERA CAMPOS DO BUFFER DE ESCRITA QUE NÃO SÃO DE INTERESSE
@@ -382,9 +376,6 @@ void MainWindow::on_Girar_clicked()
     }
     QThread::sleep(1); // 1 seg de pause TEMPO EM QUE O ROBÔ FICA GIRANDO
     write_Data(); // comando para parar
-    for (int i = 0 ; i <10 ; i++){
-        serialPort->waitForBytesWritten(100);
-    }
     read_Data();
 }
 
@@ -392,23 +383,14 @@ void MainWindow::on_Girar_clicked()
 
 void MainWindow::on_navegar_clicked()
 {
-    float inicio;
-    float fim;
-    float tempo_medido = 0.0;
-    //while(tempo_medido<5.0){
     if(flag_navegacao==0){
         ui->navegar->setText("Parar navegação");
         flag_navegacao = 1;
-
         int index = ui->Select_Robot->currentIndex(); //Seleciona o Index
+        write_buf[0] = converter_write(index + 250);
         write_buf[2*index + 1] = converter_write(ui->verticalSlider_vel_L->value()); // VELOCIDADE ESQUERDA DO ROBÔ(ID)
         write_buf[2*index + 2] = converter_write(ui->verticalSlider_vel_R->value()); // VELOCIDADE DIREITA DO ROBÔ(ID)
-        inicio = clock();
-        serialPort->flush();
         write_Data();
-        QThread::msleep(100); // 1 ms de pause
-        //serialPort->waitForReadyRead(100);
-        read_Data();
         read_Data();
     }
         else
@@ -416,15 +398,6 @@ void MainWindow::on_navegar_clicked()
         ui->navegar->setText("Navegar");
         flag_navegacao = 0;
     }
-
-
-            //if(ui->verticalSlider_vel_L->SliderValueChange() || ui->verticalSlider_vel_R->SliderValueChange()){
-              //  write_Data(); // atualiza comando
-            //}
-            //if(ui->spinBox_vel_L->valueChanged())
-        fim = clock();
-        tempo_medido = tempo_medido +(fim-inicio);
-    //}
 }
 
 
